e8baf6096696269b4951f8c072e4bc6a8019b5e0
хакер 007 <student00009662@gmail.com>
2022-06-16 20:13:11 +0500

    web app

---
+++ README.md
@@ -0,0 +1,35 @@
+# Spendings application
+
+This app allows users to track Spendings.
+
+#### Intructions 
+1.Download the source code
+
+```bash
+git clone https://github.com/adoublebOS/webtech_refer.git
+```
+or just download the *.zip file
+
+2.Install deps
+```bash
+npm install
+```
+
+3.Run the app
+```bash
+node app
+```
+
+#### Deps list
+- express
+- pug
+
+#### Link to github repo
+https://github.com/adoublebOS/webtech_refer.git
+
+#### Link to application on Glitch
+
+
+
+
+
---
+++ app.js
@@ -0,0 +1,114 @@
+const express = require('express')
+const app = express()
+
+const fs = require('fs')
+const PORT = 4000
+
+app.set('view engine', 'pug')
+app.use('/static', express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+  fs.readFile('./data/todos.json', (err, data) => {
+    if (err) throw err
+
+    const todos = JSON.parse(data)
+
+    res.render('home', { todos: todos })
+  })
+})
+
+app.post('/add', (req, res) => {
+  const formData = req.body
+
+  if (formData.todo.trim() == '') {
+    fs.readFile('./data/todos.json', (err, data) => {
+      if (err) throw err
+
+      const todos = JSON.parse(data)
+
+      res.render('home', { error: true, todos: todos })
+    })
+  } else {
+    fs.readFile('./data/todos.json', (err, data) => {
+      if (err) throw err
+
+      const todos = JSON.parse(data)
+
+      const todo = {
+        id: id(),
+        description: formData.todo,
+        done: false
+      }
+
+      todos.push(todo)
+
+      fs.writeFile('./data/todos.json', JSON.stringify(todos), (err) => {
+        if (err) throw err
+
+        fs.readFile('./data/todos.json', (err, data) => {
+          if (err) throw err
+
+          const todos = JSON.parse(data)
+
+          res.render('home', { success: true, todos: todos })
+        })
+      })
+    })
+  }
+})
+
+app.get('/:id/delete', (req, res) => {
+  const id = req.params.id
+
+  fs.readFile('./data/todos.json', (err, data) => {
+    if (err) throw err
+
+    const todos = JSON.parse(data)
+
+    const filteredTodos = todos.filter(todo => todo.id != id)
+
+    fs.writeFile('./data/todos.json', JSON.stringify(filteredTodos), (err) => {
+      if (err) throw err
+
+      res.render('home', { todos: filteredTodos, deleted: true })
+    })
+  })
+})
+
+
+app.get('/:id/update', (req, res) => {
+  const id = req.params.id
+
+  fs.readFile('./data/todos.json', (err, data) => {
+    if (err) throw err
+    
+    const todos = JSON.parse(data)
+    const todo = todos.filter(todo => todo.id == id)[0]
+    
+    const todoIdx = todos.indexOf(todo)
+    const splicedTodo = todos.splice(todoIdx, 1)[0]
+    
+    splicedTodo.done = true
+    
+    todos.push(splicedTodo)
+
+    fs.writeFile('./data/todos.json', JSON.stringify(todos), (err) => {
+      if (err) throw err
+
+      res.render('home', { todos: todos })
+    })
+  })
+    
+})
+
+app.listen(PORT, (err) => {
+  if (err) throw err
+
+  console.log(`port number - ${ PORT }`)
+})
+
+
+function id () {
+  return '_' + Math.random().toString(36).substr(2, 9);
+}
